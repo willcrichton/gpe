8988de44431a1fbcc7e617b89532a9abf810d894
Will Crichton <willcrichton@cmu.edu>
2014-11-23 16:31:05 -0500

    Tweaked constants

--- src/constants.rs
+++ src/constants.rs
@@ -7,17 +7,17 @@ pub static MUTATIONS: uint = 5;
 pub static POPULATION_SIZE: uint = 10;
 pub static VERTICES: uint = 8;
 
-pub static ADD_POLYGON_RATE: uint = 700;
-pub static REMOVE_POLYGON_RATE: uint = 1500;
+pub static ADD_POLYGON_RATE: uint = 400;
+pub static REMOVE_POLYGON_RATE: uint = 1200;
 
-pub static CHANGE_COLOR_RATE: uint = 1500;
+pub static CHANGE_COLOR_RATE: uint = 1200;
 pub static CHANGE_COLOR_MAX: f32 = 60.0;
 
-pub static MOVE_VERTEX_RATE: uint = 1500;
+pub static MOVE_VERTEX_RATE: uint = 1000;
 pub static MOVE_VERTEX_MAX: f32 = 60.0;
 
-pub static ADD_VERTEX_RATE: uint = 1500;
-pub static REMOVE_VERTEX_RATE: uint = 1500;
+pub static ADD_VERTEX_RATE: uint = 1000;
+pub static REMOVE_VERTEX_RATE: uint = 1200;
 
 #[inline(always)]
 pub fn should_mutate(max: uint) -> bool {
